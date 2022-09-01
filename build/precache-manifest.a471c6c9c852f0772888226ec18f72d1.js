62f6eb8168b536185d15fe74142dd8db7947c2cb
Thales <thales.souz@outlook.com>
2022-08-31 21:20:07 -0300

    build: building

---
+++ build/precache-manifest.a471c6c9c852f0772888226ec18f72d1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c14a6c58636d40f6a7a",
+    "url": "/static/css/main.930b2a42.chunk.css"
+  },
+  {
+    "revision": "9c14a6c58636d40f6a7a",
+    "url": "/static/js/main.5dfe8767.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bd0a6b17e760c5167a31",
+    "url": "/static/js/2.23ae4e07.chunk.js"
+  },
+  {
+    "revision": "b0eb537d5957906468d4387699281115",
+    "url": "/static/media/Mercurio.b0eb537d.png"
+  },
+  {
+    "revision": "b94d13fa86e6789be2f95eba5ad41f6b",
+    "url": "/static/media/Venus.b94d13fa.png"
+  },
+  {
+    "revision": "4aab371bfedf0b88331bcfe187bff740",
+    "url": "/static/media/Terra.4aab371b.png"
+  },
+  {
+    "revision": "e113f87a7a7fc1133a61de5f03f06e21",
+    "url": "/static/media/Marte.e113f87a.png"
+  },
+  {
+    "revision": "2a3057d6c2e210a896321a8933a63224",
+    "url": "/static/media/Jupiter.2a3057d6.png"
+  },
+  {
+    "revision": "6ee7ceec51b219191a1531757a9d0eca",
+    "url": "/static/media/Saturno.6ee7ceec.png"
+  },
+  {
+    "revision": "389d9ec692b27f7a38836c460830525a",
+    "url": "/static/media/Urano.389d9ec6.png"
+  },
+  {
+    "revision": "a929046c0c56f5f41290c580542c8ebf",
+    "url": "/static/media/Netuno.a929046c.png"
+  },
+  {
+    "revision": "76369bb36755c273936d13b632963bd2",
+    "url": "/static/media/background.76369bb3.png"
+  },
+  {
+    "revision": "de0b3c712c3bed605cdf2ab1e3d91169",
+    "url": "/index.html"
+  }
+];
